19e8be65a64eb085a4d611a66a23b149f218d5ce
Meza A <albertog1meza@gmail.com>
2023-09-19 08:38:30 -0600

    Primer inicio, funciona el sonido

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+// Importar la biblioteca de audio: En el componente de React donde deseas reproducir el sonido, 
+// importa la biblioteca de audio howler.js
+import { Howl } from 'howler';
+
 
 function App() {
+
+  const sound = new Howl({
+    src: ['assets/sonido.mp3'],
+    ///home/sparrow/SIMEI/better/public/assets/sonido.mp3
+  });
+
+   // Función que reproduce el sonido
+   const playSound = () => {
+    // // Verifica si el contexto de audio está suspendido y lo reanuda si es necesario
+    // if (Howler.ctx && Howler.ctx.state === 'suspended') {
+    //   Howler.ctx.resume().then(() => {
+    //     sound.play();
+    //   });
+    // } else {
+      sound.play();
+    // }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <input type='text' name='BarraDeProducto' placeholder='Producto escaneado'/>
+        <button onClick={playSound}>Reproducir Sonido</button>
       </header>
     </div>
   );
